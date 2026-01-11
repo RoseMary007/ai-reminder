2cdcc677d04fa8d2ed029b037ce6cf219f6ef3c6
Rose Mary <rosemaryjoe007@gmail.com>
2026-01-11 21:51:07 +0530

    updated for versal

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ scheduler_thread.start()
 
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
